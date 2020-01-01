7a9418d8aff2eab17d0a737e08e056209ecf0b78
Ondrej Cifka <cifkao@users.noreply.github.com>
2020-01-01 20:11:41 +0100

    Add models.common

---
+++ code/groove2groove/models/common.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import logging
+
+import tensorflow as tf
+
+from museflow.components import Component, using_scope
+from museflow.config import configurable
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@configurable(['2d_layers', '1d_layers'])
+class CNN(Component):
+
+    def __init__(self, training=None, name='cnn'):
+        Component.__init__(self, name=name)
+
+        self._is_training = training
+        with self.use_scope():
+            self._layers_2d = self._cfg['2d_layers'].maybe_configure() or []
+            self._layers_1d = self._cfg['1d_layers'].maybe_configure() or []
+
+    def __call__(self, inputs):
+        return self.apply(inputs)
+
+    @using_scope
+    def apply(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        features = inputs
+        if self._layers_2d:
+            if features.shape.ndims == 3:
+                # Expand to 4 dimensions: [batch_size, rows, time, channels]
+                features = tf.expand_dims(features, -1)
+
+            # 2D layers: 4 -> 4 dimensions
+            for layer in self._layers_2d:
+                _LOGGER.debug(f'Inputs to layer {layer} have shape {features.shape}')
+                features = self._apply_layer(layer, features)
+            _LOGGER.debug(f'After the 2D layers, the features have shape {features.shape}')
+
+            # Features have shape [batch_size, rows, time, channels]. Switch rows and cols, then
+            # flatten rows and channels to get 3 dimensions: [batch_size, time, new_channels].
+            features = tf.transpose(features, perm=[0, 2, 1, *range(3, features.shape.ndims)])
+            num_channels = features.shape[2] * features.shape[3]
+            features = tf.reshape(features, [batch_size, -1, num_channels])
+
+        # 1D layers: 3 -> 3 dimensions: [batch_size, time, channels]
+        for layer in self._layers_1d:
+            _LOGGER.debug(f'Inputs to layer {layer} have shape {features.shape}')
+            features = self._apply_layer(layer, features)
+
+        return features
+
+    def _apply_layer(self, layer, features):
+        if isinstance(layer, (tf.layers.Dropout, tf.keras.layers.Dropout)):
+            return layer(features, training=self._is_training)
+        return layer(features)
